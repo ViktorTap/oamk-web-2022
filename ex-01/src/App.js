9b4ec2def89eacb5087c69ed1ba9a327b2cf5c11
AB4380 <ab4380@student.jamk.fi>
2022-01-18 11:55:31 +0200

    tehtävä tehty vaikka ei täydellisesti

--- ex-01/src/App.js
+++ ex-01/src/App.js
@@ -44,6 +44,7 @@ function App() {
             <li><a href='#Kulttuuri'>Kulttuuri</a></li>
           </ul>
         </div>
+
         <div className='main-uutiset'>
           <div>
             <p>Uutinen numero 1</p>
@@ -55,11 +56,24 @@ function App() {
             <p>Uutinen numero 3</p>
           </div>
         </div>
-        <div className = 'main-section'>
-        </div>
-        <div className='main-aside'>
 
+        <div className = 'main-section'>
+          <div className='main-item one'>
+            <p>"Sed ut perspiciatis unde omnis iste natus error sit 
+              voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis 
+              et quasi architecto beatae vitae dicta sunt explicabo.</p>
+              <img src='https://images.unsplash.com/photo-1572949645841-094f3a9c4c94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'></img>
+          </div>
+          <div className='main-item'>2
+            <ol className='ordered-list'>
+              <li>Uutinen numero 1</li>
+              <li>Uutinen numero 2</li>
+              <li>Uutinen numero 3</li>
+              <li>Uutinen numero 4</li>
+            </ol>
+          </div>          
         </div>
+
       </body>
     </div>
   );
