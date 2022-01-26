0a0e5b9e35ed513e89b960c1c948678fccbe7564
AB4380 <ab4380@student.jamk.fi>
2022-01-26 11:39:50 +0200

    small changes

--- ex-02/src/App.js
+++ ex-02/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Title from './components/Title';
-import ShoppingList from './components/ShoppingList';
-import styles from './App.module.css';
-import './App.css';
+import Title from "./components/Title";
+import ShoppingList from "./components/ShoppingList";
+import Button from "./components/Button";
+import styles from "./App.module.css";
+import "./App.css";
 
 /* A ES6 class style stateful component for the shopping list application */
 class App extends React.Component {
-  constructor(props)
-  {
+  constructor(props) {
     /* You should call super(props) before any other statement. 
        Otherwise, this.props will be undefined in the constructor, which can lead to bugs.
     */
@@ -15,25 +15,28 @@ class App extends React.Component {
 
     this.state = {
       items: [
-        { id: 1, value: 'Milk', qty: 5, unit: 'ltr' },
-        { id: 2, value: 'Bananas', qty: 6, unit: 'pcs' },
-        { id: 3, value: 'Bread', qty: 3, unit: 'x' },
-        { id: 4, value: 'Eggs', qty: 16, unit: 'x' }
-      ]
+        { id: 1, value: "Milk", qty: 5, unit: "ltr" },
+        { id: 2, value: "Bananas", qty: 6, unit: "pcs" },
+        { id: 3, value: "Bread", qty: 3, unit: "x" },
+        { id: 4, value: "Eggs", qty: 16, unit: "x" },
+      ],
     };
-
   }
 
-  render()
-  {
+  render() {
     const { applicationDescription, applicationName } = this.props;
-    return <div className={ styles.shoppingList }>
-      <Title 
-        applicationDescription={ applicationDescription }
-        applicationName={ applicationName }
-      />
-      <ShoppingList items={ this.state.items } />
-    </div>
+    return (
+      <div className="main-container">
+        <div className={styles.shoppingList}>
+          <Title
+            applicationDescription={applicationDescription}
+            applicationName={applicationName}
+          />
+          <ShoppingList items={this.state.items} />
+          <Button />
+        </div>
+      </div>
+    );
   }
 }
 
---
+++ ex-02/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Button() {
+  return (
+    <div>
+      <button type="submit">Add Milk</button>
+    </div>
+  );
+}
+
+export default Button;
